79aff9fc93bfcc3e7950d70080282b45746f7bdb
Milos Milin <milos.m@smtps.eu>
2020-05-27 16:30:55 +0200

    fix

--- deploy.sh
+++ deploy.sh
@@ -23,7 +23,7 @@ function deploy() {
 }
 
 if [ ${BRANCH} = "master" ]; then
-	deploy "https://milosm-deploy.vivifyideas.com" 'milosm'
+	deploy "https://milosm-deploy.vivifyideas.com" 'laravel'
 else
 	echo "Branch ${BRANCH} is not deployable"
 fi
